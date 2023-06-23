66adbe0ff15333f82c6496ed78dd7512d1a22e25
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-06-23 17:18:38 +0545

    React Project init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./component/HomePage/Home";
+import { TopHeader } from "./component/HomePage/TopHeader";
+import Footer from "./component/HomePage/Footer";
+import { Error404 } from "./component/ErrorPage/Error404";
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <TopHeader/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
+  )
+}
+
+export default App
